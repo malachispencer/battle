412789016c47a58b572973cdf681e7f45b9cfe5c
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-11 20:22:46 +0000

    Implemented hit points feature

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,13 +1,14 @@
 feature 'attack player 2' do
-  scenario 'player 1 attacks player 2 and wins' do
+  scenario 'player 1 attacks player 2' do
     sign_in_and_play
     click_button('attack')
     expect(page).to have_content('AI attacks Human')
   end
 
-  scenario 'attack button reduces hit points ' do
+  scenario 'attack button reduces hit points' do
     sign_in_and_play
     click_button('attack')
+    click_link('ok')
     expect(page).not_to have_content('Human 60HP')
     expect(page).to have_content('Human 50HP')
   end
