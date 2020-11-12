4d8cc1c3915c7525614eeaee6c56c80f71900a38
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-12 08:25:05 +0000

    player_one hit points made visible feature test and feature added

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,5 +1,10 @@
 feature 'View hit points' do
-  scenario 'see Player 2 hit points' do
+  scenario 'see player_one hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'AI 60HP'
+  end
+
+  scenario 'see player_one hit points' do
     sign_in_and_play
     expect(page).to have_content 'Human 60HP'
   end
